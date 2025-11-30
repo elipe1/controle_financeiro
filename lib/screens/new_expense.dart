9fa5a48469c9791a9eeed8cc73c3a01d44102c3b
Luiz Phelippe <aguiarluizphelippe@gmail.com>
2025-11-30 18:43:36 -0300

    feat: update new_expense screen

--- lib/screens/new_expense.dart
+++ lib/screens/new_expense.dart
@@ -9,7 +9,7 @@ class Expense extends StatefulWidget {
 }
 
 class _ExpenseState extends State<Expense> {
-  final TextEditingController title = TextEditingController();
+  final TextEditingController description = TextEditingController();
   final TextEditingController value = TextEditingController();
   DateTime? date;
   String? category;
@@ -22,19 +22,28 @@ class _ExpenseState extends State<Expense> {
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
       ),
+      
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              Text ("Digite os dados do novo gasto:", 
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+
+              const SizedBox(height: 16),
               TextField(
-                controller: title,
+                controller: description,
                 decoration: const InputDecoration(
-                  labelText: 'Título',
+                  labelText: 'Descrição',
                   border: OutlineInputBorder(),
                 ),
               ),
+
               const SizedBox(height: 16),
               TextField(
                 controller: value,
@@ -44,6 +53,53 @@ class _ExpenseState extends State<Expense> {
                   border: OutlineInputBorder(),
                 ),
               ),
+
+              const SizedBox(height: 16),
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text ("Data do gasto:",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  ),
+                  ElevatedButton(
+                  onPressed: () async {
+                    final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                    builder: (context, child) {
+                      return Theme(
+                        data: Theme.of(context).copyWith(
+                          colorScheme: ColorScheme.light(
+                            primary: Colors.green,
+                            onPrimary: Colors.white,
+                            onSurface: Colors.black,
+                          ),
+                          textButtonTheme: TextButtonThemeData(
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.green,
+                            ),
+                          ),
+                        ),
+                        child: child!,
+                      );
+                    },
+                    );
+                    if (selectedDate != null) {
+                    setState(() {
+                      date = selectedDate;
+                    });
+                    }
+                  },
+                  child: Text(date == null
+                    ? 'Selecionar Data'
+                    : 'Data: ${date!.day}/${date!.month}/${date!.year}'),
+                  ),
+                ],
+                ),
             ],
           ),
         ),
